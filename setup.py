b8f3b85ddb4fd0d37fb0883195929e8d0c913611
Brendon Ky <brendonky18@gmail.com>
2022-03-25 21:55:44 -0400

    idk anymore

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='debugger',
+    name='py_debugger',
     version='0.0.1',
     description='Simple python program to make printing, logging and debugging easier.',
     url='git@github.com:brendonky18/PyDebugger.git',
     author='Brendon Ky',
     author_email='brendonky28@gmail.com',
     license='unlicense',
-    packages=['debugger'],
+    packages=['_debugger'],
     # install_requires=['curses'],
     zip_safe=False
 )
